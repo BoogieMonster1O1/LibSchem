fde4c5991836c43df121631f0e949352e2f18f96
SD <harrypotter.starwars.shrish@gmail.com>
2020-09-28 22:36:22 +0530

    Initial commit

---
+++ src/main/java/io/github/boogiemonster1o1/libschem/LibSchem.java
@@ -0,0 +1,10 @@
+package io.github.boogiemonster1o1.libschem;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LibSchem implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
