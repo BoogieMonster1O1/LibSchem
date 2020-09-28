530f15a8517e2c1bf5981fdf48a6953a0bf01e89
SD <harrypotter.starwars.shrish@gmail.com>
2020-09-28 22:55:08 +0530

    Test
     Changes to be committed:
    	modified:   src/main/java/io/github/boogiemonster1o1/libschem/impl/LibSchem.java
    	new file:   src/main/java/io/github/boogiemonster1o1/libschem/impl/SchematicTest.java

--- src/main/java/io/github/boogiemonster1o1/libschem/impl/LibSchem.java
+++ src/main/java/io/github/boogiemonster1o1/libschem/impl/LibSchem.java
@@ -1,12 +1,35 @@
 package io.github.boogiemonster1o1.libschem.impl;
 
+import java.io.IOException;
+
 import org.apache.logging.log4j.LogManager;
 
+import net.minecraft.server.MinecraftServer;
+
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class LibSchem implements ModInitializer {
+    static MinecraftServer server;
+
     @Override
     public void onInitialize() {
         LogManager.getLogger().info("Initializing LibSchem - Powered by DataFixerUpper");
+
+        if (FabricLoader.getInstance().isModLoaded("fabric")) {
+            this.doFapiStuff();
+        }
+    }
+
+    private void doFapiStuff() {
+        ServerLifecycleEvents.SERVER_STARTING.register(server -> LibSchem.server = server);
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            try {
+                SchematicTest.test();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
